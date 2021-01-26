7d7a95e0621706386cf22e38d99c85ee029ee955
Frans-Lukas Lövenvald <franslukas@gmail.com>
2021-01-26 14:48:58 +0100

    added some comments

--- hdfs_python_demo/main.py
+++ hdfs_python_demo/main.py
@@ -1,15 +1,16 @@
 from hdfs import Client
 from hdfs import Config
 
-
+# overwrites model with name "model.json" to hdfs storage
+# model is taken from local model.json file located in same folder as this script.
 def save_model(client: Client):
     with open('model.json') as model, client.write('model.json', overwrite=True, encoding='utf-8') as writer:
         from json import dump
         dump(model.read(), writer)
 
-
+# reads model with name "model.json" from hdfs storage
 def read_model(client: Client) -> str:
-    with client.read('model2.json', encoding='utf-8') as reader:
+    with client.read('model.json', encoding='utf-8') as reader:
         from json import load
         model = load(reader)
     return model
