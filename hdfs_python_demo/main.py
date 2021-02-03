de519faae55bfc02fa807f58599c0d77533e438f
franslukas <franslukas@gmail.com>
2021-02-03 11:28:29 +0100

    test hdfs now works!

--- hdfs_python_demo/main.py
+++ hdfs_python_demo/main.py
@@ -1,10 +1,11 @@
 import os
 
-from hdfs import Client, InsecureClient
+from hdfs import Client, Config
 
 # overwrites model with name "model.json" to hdfs storage
 # model is taken from local model.json file located in same folder as this script.
 ADDRESS = "http://192.168.10.205:9870"
+test_client = Config().get_client('dev')
 
 
 def save_model(client: Client):
@@ -17,19 +18,9 @@ def test_hdfs(context, event):
                              trigger_kind=event.trigger.kind,
                              event_body=event.body,
                              some_env=os.environ.get('MY_ENV_VALUE'))
-    # onlyfiles = [f for f in os.listdir(".") if os.path.isfile(os.path.join(".", f))]
-    # return onlyfiles
-    # with open("/tmp/model.json", "r") as f:
-    #     return f.read()
-    try:
-        client.write('model.json', overwrite=True, encoding='utf-8')
-        test_client = InsecureClient(ADDRESS, "franslukas")
-        save_model(test_client)
-    except Exception as e:
-        return e
+
+    save_model(test_client)
     return read_model(test_client)
-    # import getpass
-    # return getpass.getuser()
 
 
 # reads model with name "model.json" from hdfs storage
@@ -45,6 +36,5 @@ def read_model(client: Client) -> str:
 # defaults to ~/.hdfscli.cfg
 if __name__ == '__main__':
     # client = Config().get_client('dev')
-    client = InsecureClient(ADDRESS, "franslukas")
-    save_model(client)
-    print(read_model(client))
+    save_model(test_client)
+    print(read_model(test_client))
