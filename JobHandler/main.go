af4a2b2c5e6b206dae7ccae33e66399ddc6c8216
tfy15jst <johansundqvist96@gmail.com>
2021-03-01 15:19:44 +0100

    added in config functionality to jobHandler

--- JobHandler/main.go
+++ JobHandler/main.go
@@ -18,11 +18,17 @@ var clientSet *kubernetes.Clientset
 func main() {
 
 	// 1. receive job
-	if len(os.Args) < 3 {
-		log.Fatalf("wrong input, needs arguments <jobPath> and <pathToCfg>")
+	if len(os.Args) < 2 {
+		log.Fatalf("wrong input, needs arguments <jobPath> and optional <pathToCfg>")
 	}
 
-	err := initializeClients(os.Args[2])
+	var err error
+	if len(os.Args) > 2 {
+		err = initializeClients(os.Args[2])
+	} else {
+		err = initializeClients("")
+	}
+
 	helperFunctions.FatalErrCheck(err, "main: ")
 
 	// 2. Parse to Job Class
