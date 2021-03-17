3b76b12484b8e9f4518c0ae41a1ae9d4beffa3ed
Frans-Lukas <franslukas@pm.me>
2021-03-17 10:50:41 +0100

    added better prints

--- setupScripts/reset_minikube.sh
+++ setupScripts/reset_minikube.sh
@@ -8,3 +8,5 @@ kubectl apply -f https://raw.githubusercontent.com/nuclio/nuclio/master/hack/k8s
 kubectl apply -f https://raw.githubusercontent.com/nuclio/nuclio/master/hack/k8s/resources/nuclio.yaml
 sleep 5
 kubectl get pods --namespace nuclio
+
+nuctl deploy helloworld -n nuclio -p https://raw.githubusercontent.com/nuclio/nuclio/master/hack/examples/golang/helloworld/helloworld.go --http-trigger-service-type=NodePort --registry docker.io/franslukas
