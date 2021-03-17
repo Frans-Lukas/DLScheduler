ca0c641993d4fc4e4581e96cb9ad29899be0b1da
Frans-Lukas <franslukas@pm.me>
2021-03-17 13:38:01 +0100

    fixed print again

--- MXNetImplementations/LeNet/MXNet/shell_mxnet/build_docker_container.sh
+++ MXNetImplementations/LeNet/MXNet/shell_mxnet/build_docker_container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 nuctl build mxnet_sh --path . --registry docker.io/franslukas
+nuctl deploy helloworld -n nuclio -p https://raw.githubusercontent.com/nuclio/nuclio/master/hack/examples/golang/helloworld/helloworld.go --http-trigger-service-type=NodePort --registry docker.io/franslukas
