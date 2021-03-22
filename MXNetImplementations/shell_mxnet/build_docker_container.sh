ad50a572ae67da774e1666adc4e168529e475c53
tfy15jst <johansundqvist96@gmail.com>
2021-03-22 10:26:53 +0100

    fixed infinite loop

--- MXNetImplementations/shell_mxnet/build_docker_container.sh
+++ MXNetImplementations/shell_mxnet/build_docker_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nuctl build mxnet_sh --path . --registry docker.io/franslukas
+nuctl build mxnet_sh --path . --registry docker.io/johansundkvist
 #nuctl deploy helloworld -n nuclio -p https://raw.githubusercontent.com/nuclio/nuclio/master/hack/examples/golang/helloworld/helloworld.go --http-trigger-service-type=NodePort --registry docker.io/franslukas
