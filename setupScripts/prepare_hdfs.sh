3deb909550788db83b161abb25a109cfb86afcaa
Frans-Lukas <franslukas@pm.me>
2021-03-26 12:04:52 +0100

    updated prepare_hdfs to use /usr/bin/hadoop-3.3.0 env

--- setupScripts/prepare_hdfs.sh
+++ setupScripts/prepare_hdfs.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-  echo "usage: ./prepare_hdfs.sh \$USER"
-fi
-
 hdfs dfs -rm -r "/*"
 hdfs namenode -format
-/usr/bin/hadoop/sbin/stop-all.sh
-/usr/bin/hadoop/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/stop-all.sh
+$HADOOP_HOME/sbin/start-dfs.sh
 hadoop fs -mkdir /user
 hadoop fs -mkdir /user/franslukas
 hadoop fs -mkdir /user/franslukas/models
