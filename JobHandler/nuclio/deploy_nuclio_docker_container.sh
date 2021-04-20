155e9271308f69d10d5121e1bc997b4b2e6d6944
Frans-Lukas <franslukas@pm.me>
2021-04-20 11:26:58 +0200

    removed sudo from invoke and deploy (nuctl)

--- JobHandler/nuclio/deploy_nuclio_docker_container.sh
+++ JobHandler/nuclio/deploy_nuclio_docker_container.sh
@@ -3,4 +3,4 @@
 # make sure rbac roles have been applied (see https://nuclio.io/docs/latest/setup/k8s/getting-started-k8s/)
 # kubectl apply -f https://raw.githubusercontent.com/nuclio/nuclio/master/hack/k8s/resources/nuclio-rbac.yaml
 echo "deploying with args: $1 $2"
-sudo nuctl deploy $1 --run-image $2 --file nuclio/function.yaml -n nuclio --http-trigger-service-type=NodePort
+nuctl deploy $1 --run-image $2 --file nuclio/function.yaml -n nuclio --http-trigger-service-type=NodePort
--- JobHandler/nuclio/invoke_nuclio_function.sh
+++ JobHandler/nuclio/invoke_nuclio_function.sh
@@ -4,11 +4,11 @@ if [[ $# -eq 6 ]]; then
   echo "invoking with 6 arg"
   echo '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'"}'
   echo 'arg 7: '$7''
-  sudo nuctl invoke $1 -n nuclio -b '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'"}'
+  nuctl invoke $1 -n nuclio -b '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'"}'
 elif [[ $# -eq 7 ]]; then
   echo "invoking with 7 arg"
   echo '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'", "num_parts": "'$7'"}'
-  sudo nuctl invoke $1 -n nuclio -b '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'", "num_parts": "'$7'"}'
+  nuctl invoke $1 -n nuclio -b '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'", "num_parts": "'$7'"}'
 fi
 
 #echo '{"ip": "'$2'", "role": "'$3'", "num_worker": '$4', "num_server": '$5', "script_path": "'$6'", "num_parts": '$7'}'
