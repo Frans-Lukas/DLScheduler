59e511a9b86c07678426ca1a89ae93ae2dfb72f0
Frans-Lukas <franslukas@pm.me>
2021-04-23 14:02:27 +0200

    added timeout to exec command

--- JobHandler/helperFunctions/shellFunctions.go
+++ JobHandler/helperFunctions/shellFunctions.go
@@ -2,11 +2,16 @@ package helperFunctions
 
 import (
 	"bytes"
+	"context"
 	"os/exec"
+	"time"
 )
 
 func ExecuteFunction(name string, args ...string) (bytes.Buffer, bytes.Buffer, error) {
-	cmd := exec.Command(name, args...)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel() // The cancel should be deferred so resources are cleaned up
+
+	cmd := exec.CommandContext(ctx, name, args...)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
