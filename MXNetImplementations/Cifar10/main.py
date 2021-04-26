83f9f088a284de0a0831a0baf7e29bbba2600159
Frans-Lukas <franslukas@pm.me>
2021-04-26 15:54:10 +0200

    added asdfsdfg

--- MXNetImplementations/Cifar10/main.py
+++ MXNetImplementations/Cifar10/main.py
@@ -26,12 +26,11 @@ import random
 import re
 
 import mxnet as mx
+import mxnet.ndarray as F
 import numpy as np
 from mxnet import autograd, gluon, kv, nd
-from mxnet.gluon.model_zoo import vision
-from mxnet.gluon.model_zoo.vision import ResNetV1
 from mxnet.gluon import nn
-import mxnet.ndarray as F
+from mxnet.gluon.model_zoo import vision
 
 from cloudStorage import download_simple, upload_simple
 
@@ -61,6 +60,7 @@ class Net(gluon.Block):
         x = F.tanh(self.fc2(x))
         return x
 
+
 def save_model_to_gcloud(net: Net):
     net.save_params(MODEL_WEIGHTS_PATH)
     name = get_weights_file_name()
@@ -87,24 +87,6 @@ def load_model(net: Net):
     return net
 
 
-def get_mnist_iterator_container(batch_size, input_shape, num_parts=1, part_index=0):
-    """Returns training and validation iterators for MNIST dataset
-    """
-    flat = len(input_shape) != 3
-
-    train_dataiter = mx.io.MNISTIter(
-        image="/opt/nuclio/data/train-images-idx3-ubyte",
-        label="/opt/nuclio/data/train-labels-idx1-ubyte",
-        input_shape=input_shape,
-        batch_size=batch_size,
-        shuffle=True,
-        flat=flat,
-        num_parts=num_parts,
-        part_index=part_index)
-
-    return train_dataiter
-
-
 def main():
     # Create a distributed key-value store
     store = kv.create('dist')
@@ -115,7 +97,7 @@ def main():
     # 64 images in a batch
     batch_size_per_gpu = 64
     # How many epochs to run the training
-    epochs = 10
+    epochs = 1
 
     # How many GPUs per machine
     gpus_per_machine = 1
@@ -125,23 +107,95 @@ def main():
     # Create the context (a list of all GPUs to be used for training)
     ctx = [mx.gpu() if mx.test_utils.list_gpus() else mx.cpu()]
 
+    # Convert to float 32
+    # Having channel as the first dimension makes computation more efficient. Hence the (2,0,1) transpose.
+    # Dividing by 255 normalizes the input between 0 and 1
+    def transform(data, label):
+        return nd.transpose(data.astype(np.float32), (2, 0, 1)) / 255, label.astype(np.float32)
+
+    class SplitSampler(gluon.data.sampler.Sampler):
+        """ Split the dataset into `num_parts` parts and sample from the part with index `part_index`
+
+        Parameters
+        ----------
+        length: int
+          Number of examples in the dataset
+        num_parts: int
+          Partition the data into multiple parts
+        part_index: int
+          The index of the part to read from
+        """
+
+        def __init__(self, length, num_parts=1, part_index=0):
+            # Compute the length of each partition
+            self.part_len = length // num_parts
+            # Compute the start index for this partition
+            self.start = self.part_len * part_index
+            # Compute the end index for this partition
+            self.end = self.start + self.part_len
+
+        def __iter__(self):
+            # Extract examples between `start` and `end`, shuffle and return them.
+            indices = list(range(self.start, self.end))
+            random.shuffle(indices)
+            return iter(indices)
+
+        def __len__(self):
+            return self.part_len
+
     num_parts = os.getenv("NUM_PARTS")
     if num_parts is None or num_parts == 1:
         num_parts = store.num_workers
     else:
         num_parts = int(num_parts)
     # Load the training data
-    train_data = get_mnist_iterator_container(batch_size, (1, 28, 28), num_parts=num_parts, part_index=store.rank)
+    train_data = gluon.data.DataLoader(
+        gluon.data.vision.CIFAR10(train=True, root="/opt/nuclio/data").transform(transform), batch_size,
+        sampler=SplitSampler(5000, num_parts, store.rank))
 
     # Use ResNet from model zoo
-    net = Net()
+    net = vision.resnet18_v1()
 
     # Initialize the parameters with Xavier initializer
     net.collect_params().initialize(mx.init.Xavier(), ctx=ctx)
     load_model(net)
 
     # Use Adam optimizer. Ask trainer to use the distributor kv store.
-    trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': .001}, kvstore=store)
+    trainer = gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': .001}, kvstore=store)
+
+    # Evaluate accuracy of the given network using the given data
+    def evaluate_accuracy(data_iterator, network):
+        """ Measure the accuracy of ResNet
+
+        Parameters
+        ----------
+        data_iterator: Iter
+          examples of dataset
+        network:
+          ResNet
+
+        Returns
+        ----------
+        tuple of array element
+        """
+        acc = mx.metric.Accuracy()
+
+        # Iterate through data and label
+        for i, (data, label) in enumerate(data_iterator):
+            # Get the data and label into the GPU
+            data = data.as_in_context(ctx[0])
+            label = label.as_in_context(ctx[0])
+
+            # Get network's output which is a probability distribution
+            # Apply argmax on the probability distribution to get network's classification.
+            output = network(data)
+            predictions = nd.argmax(output, axis=1)
+
+            # Give network's prediction and the correct label to update the metric
+            acc.update(preds=predictions, labels=label)
+
+        # Return the accuracy
+        return acc.get()[1]
 
     # We'll use cross entropy loss since we are doing multiclass classification
     loss = gluon.loss.SoftmaxCrossEntropyLoss()
@@ -152,15 +206,8 @@ def main():
         # Ask autograd to remember the forward pass
         with autograd.record():
             # Compute the loss on all GPUs
-            losses = []
-            outputs = []
-            for X, Y in zip(data, label):
-                z = network(X)
-                losses.append(loss(z, Y))
-                outputs.append(z)
-                # losses = [loss(network(X), Y) ]
+            losses = [loss(network(X), Y) for X, Y in zip(data, label)]
 
-        # metric.update(label, outputs)
         # Run the backward pass (calculate gradients) on all GPUs
         for l in losses:
             l.backward()
@@ -169,17 +216,20 @@ def main():
     # Train a batch using multiple GPUs
     def train_batch(batch_list, context, network, gluon_trainer):
         # Split and load data into multiple GPUs
-        data = gluon.utils.split_and_load(batch_list.data[0], ctx_list=context, batch_axis=0)
+        data = batch_list[0]
+        data = gluon.utils.split_and_load(data, context)
 
         # Split and load label into multiple GPUs
-        label = gluon.utils.split_and_load(batch_list.label[0], ctx_list=context, batch_axis=0)
+        label = batch_list[1]
+        label = gluon.utils.split_and_load(label, context)
 
         # Run the forward and backward pass
         loss = forward_backward(network, data, label)
 
         # Update the parameters
         # print(batch_list[0].shape[0])
-        gluon_trainer.step(batch_list.data[0].shape[0])
+        if batch_list[0].shape[0] == 64:
+            gluon_trainer.step(batch_list[0].shape[0])
         return loss
 
     loss_val = None
@@ -199,9 +249,9 @@ def main():
         # print("Epoch %d: Test_acc %f" % (epoch, test_accuracy))
         # sys.stdout.flush()
     loss_val = re.search('\[(.*)\]', str(loss_val)).group(1)
-    # name, acc = metric.get()
+    acc = evaluate_accuracy(train_data, net)
     print(
-        "regexpresultstart{\"loss\":" + loss_val + ", \"accuracy\":0, \"worker_id\":0}regexpresultend")
+        "regexpresultstart{\"loss\":" + loss_val + ", \"accuracy\":" + str(acc) + ", \"worker_id\":0}regexpresultend")
     save_model_to_gcloud(net)
 
 
