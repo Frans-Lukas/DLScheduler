44dd0f40216025254a3a8f08a6925964c7a077f5
tfy15jst <johansundqvist96@gmail.com>
2021-04-29 17:53:14 +0200

    run_all_tests now runs the 3w/3s tests

--- JobHandler/run_all_tests.sh
+++ JobHandler/run_all_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#TODO remember that the commented out tests need to be run again
 #./test_runner.sh 3
 #./test_runner.sh 4
 #./test_runner.sh 1
@@ -8,3 +9,5 @@
 ./test_runner.sh 6
 ./test_runner.sh 7
 ./test_runner.sh 8
+./test_runner.sh 9
+./test_runner.sh 10
