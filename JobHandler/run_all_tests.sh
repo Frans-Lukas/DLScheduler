6ad9a5ddaad09dcb0c25bff9ab2d448421eb9b7e
Frans-lukas Lovenvald <franslukas@gmail.com>
2021-04-29 23:03:47 +0200

    more test results

--- JobHandler/run_all_tests.sh
+++ JobHandler/run_all_tests.sh
@@ -3,8 +3,10 @@
 #./test_runner.sh 3
 #./test_runner.sh 4
 #./test_runner.sh 1
-./test_runner.sh 2
-./test_runner.sh 5
+#./test_runner.sh 2
+#./test_runner.sh 5
 ./test_runner.sh 6
 ./test_runner.sh 7
 ./test_runner.sh 8
+./test_runner.sh 3
+./test_runner.sh 4
