b8125edd94063d8e349f6ed45485001d300a12bb
Frans-Lukas <franslukas@pm.me>
2021-05-01 15:45:49 +0200

    set run_all_tests order

--- JobHandler/run_all_tests.sh
+++ JobHandler/run_all_tests.sh
@@ -2,38 +2,38 @@
 
 #TODO remember that the commented out tests need to be run again
 
-./test_runner.sh 3 1
-./test_runner.sh 4 1
-./test_runner.sh 1 1
-./test_runner.sh 2 1
-./test_runner.sh 5 1
-./test_runner.sh 6 1
-./test_runner.sh 7 1
-./test_runner.sh 8 1
-./test_runner.sh 9 1
-./test_runner.sh 10 1
-./test_runner.sh 11 1
-./test_runner.sh 12 1
-./test_runner.sh 13 1
-./test_runner.sh 14 1
-./test_runner.sh 15 1 #TODO has not been run yet
-./test_runner.sh 16 1 #TODO has not been run yet
+#./test_runner.sh 3 1
+#./test_runner.sh 4 1
+#./test_runner.sh 1 1
+#./test_runner.sh 2 1
+#./test_runner.sh 5 1
+#./test_runner.sh 6 1
+#./test_runner.sh 7 1
+#./test_runner.sh 8 1
+#./test_runner.sh 9 1
+#./test_runner.sh 10 1
+#./test_runner.sh 11 1
+#./test_runner.sh 12 1
+#./test_runner.sh 13 1
+#./test_runner.sh 14 1
+#./test_runner.sh 15 1 #TODO has not been run yet
+#./test_runner.sh 16 1 #TODO has not been run yet
 
 
 #TODO uncomment these to run the Cifar10 tests
-#./test_runner.sh 1 2
-#./test_runner.sh 2 2
-#./test_runner.sh 3 2
-#./test_runner.sh 4 2
-#./test_runner.sh 5 2
-#./test_runner.sh 6 2
-#./test_runner.sh 7 2
-#./test_runner.sh 8 2
+./test_runner.sh 3 2
+./test_runner.sh 4 2
+./test_runner.sh 1 2
+./test_runner.sh 2 2
+./test_runner.sh 5 2
+./test_runner.sh 6 2
+./test_runner.sh 7 2
+./test_runner.sh 8 2
 #./test_runner.sh 9 2
 #./test_runner.sh 10 2
-#./test_runner.sh 11 2
-#./test_runner.sh 12 2
+./test_runner.sh 11 2
+./test_runner.sh 12 2
 #./test_runner.sh 13 2
 #./test_runner.sh 14 2
-#./test_runner.sh 15 2
-#./test_runner.sh 16 2
+./test_runner.sh 15 2
+./test_runner.sh 16 2
