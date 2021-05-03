7f3f729e265156ccb3137dbf1be532920c5776ec
Frans-Lukas <franslukas@pm.me>
2021-05-03 10:32:22 +0200

    num_parts no longer set to 1 always

--- JobHandler/nuclio/invoke_nuclio_function.sh
+++ JobHandler/nuclio/invoke_nuclio_function.sh
@@ -2,8 +2,8 @@
 
 if [[ $# -eq 7 ]]; then
   echo "invoking with 7 arg"
-  echo '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'", "job_id": "'$7'", "num_parts": "1"}'
-  nuctl invoke $1 -n nuclio -b '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'", "job_id": "'$7'", "num_parts": "1"}'
+  echo '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'", "job_id": "'$7'"}'
+  nuctl invoke $1 -n nuclio -b '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'", "job_id": "'$7'"}'
 elif [[ $# -eq 8 ]]; then
   echo "invoking with 8 arg"
   echo '{"ip": "'$2'", "role": "'$3'", "num_worker": "'$4'", "num_server": "'$5'", "script_path": "'$6'", "job_id": "'$7'", "num_parts": "'$8'"}'
