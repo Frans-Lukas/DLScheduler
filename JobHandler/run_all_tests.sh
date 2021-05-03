68eaace995ae1497a7c713f98f4a8eec73b9cd76
Frans-lukas Lovenvald <franslukas@gmail.com>
2021-05-03 10:11:19 +0200

    added test 15 2

--- JobHandler/run_all_tests.sh
+++ JobHandler/run_all_tests.sh
@@ -16,24 +16,24 @@
 #./test_runner.sh 12 1
 #./test_runner.sh 13 1
 #./test_runner.sh 14 1
-./test_runner.sh 15 1 #TODO has not been run yet
-./test_runner.sh 16 1 #TODO has not been run yet
+#./test_runner.sh 15 1 #TODO has not been run yet
+#./test_runner.sh 16 1 #TODO has not been run yet
 
 
 #TODO uncomment these to run the Cifar10 tests
 #./test_runner.sh 3 2
 #./test_runner.sh 4 2
-#./test_runner.sh 1 2
-#./test_runner.sh 2 2
-#./test_runner.sh 5 2
-#./test_runner.sh 6 2
-#./test_runner.sh 7 2
-#./test_runner.sh 8 2
-#./test_runner.sh 9 2
-#./test_runner.sh 10 2
-#./test_runner.sh 11 2
-#./test_runner.sh 12 2
-#./test_runner.sh 13 2
-#./test_runner.sh 14 2
-#./test_runner.sh 15 2
+./test_runner.sh 1 2
+./test_runner.sh 2 2
+./test_runner.sh 5 2
+./test_runner.sh 6 2
+./test_runner.sh 7 2
+./test_runner.sh 8 2
+./test_runner.sh 9 2
+./test_runner.sh 10 2
+./test_runner.sh 11 2
+./test_runner.sh 12 2
+./test_runner.sh 13 2
+./test_runner.sh 14 2
+./test_runner.sh 15 2
 ./test_runner.sh 16 2
